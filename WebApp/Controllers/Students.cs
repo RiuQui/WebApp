c0ca56636aea8adce85530b9761947640b8a9b1b
RuiQui <rachlgly@gmail.com>
2025-04-11 11:06:33 +0800

    Add project files.

---
+++ WebApp/Controllers/Students.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class Students : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
